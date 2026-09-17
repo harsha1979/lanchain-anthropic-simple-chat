3da4d8d4e8da9084eb480d6452ec8ab873c30694
Harsha <harshat@wso2.com>
2026-09-17 00:29:39 -0500

    removing reload

--- http_app.py
+++ http_app.py
@@ -44,4 +44,4 @@ if __name__ == "__main__":
 
     host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("http_app:app", host=host, port=port, reload=True)
+    uvicorn.run("http_app:app", host=host, port=port)
